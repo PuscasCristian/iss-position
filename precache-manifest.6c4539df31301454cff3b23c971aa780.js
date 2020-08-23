b357f4ed076c31abff6f8ecbb739cc47f924a441
Puscas Cristian <pusckyh@gmail.com>
2020-08-23 13:56:42 +0300

    Updates

---
+++ precache-manifest.6c4539df31301454cff3b23c971aa780.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb8e23602b77127afc29916084f5981",
+    "url": "/iss-position/index.html"
+  },
+  {
+    "revision": "0238ab8be9d4e8993dea",
+    "url": "/iss-position/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "031d23cbdd2e470de120",
+    "url": "/iss-position/static/css/main.2105e218.chunk.css"
+  },
+  {
+    "revision": "0238ab8be9d4e8993dea",
+    "url": "/iss-position/static/js/2.edad5de8.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/iss-position/static/js/2.edad5de8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031d23cbdd2e470de120",
+    "url": "/iss-position/static/js/main.7900e11d.chunk.js"
+  },
+  {
+    "revision": "c4d363cf47aba4d22e9c",
+    "url": "/iss-position/static/js/runtime-main.198fe7ac.js"
+  }
+]);
